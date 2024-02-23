c5275ff4543162042ffa22afd741c92d3f696d6f
kanonChakma <kanon122chakma@gmail.com>
2024-02-23 23:27:19 +0600

    updated

---
+++ app/check/page.tsx
@@ -0,0 +1,12 @@
+const PDFViewer = () => {
+  return (
+    <div>
+      <iframe
+        src="https://pdf.wwwjumjournalmusic.top/wp-content/uploads/2024/02/doc6.pdf"
+        width="100%"
+        height="900px"
+      />
+    </div>
+  );
+};
+export default PDFViewer;
